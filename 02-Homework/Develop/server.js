c49cb389d7349db2306e7101259efa5396bf63ae
C. Rando <crando@gmail.com>
2021-06-24 09:12:12 -0400

    Add backend configs

---
+++ 02-Homework/Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
+
+//initialize app to create a port 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// sets up body parsing to route middleware
+
+// Adding configurations for backend 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public')); 
+
+// Assign routes for backend
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
